9d526e51239e43b389298001323ab1aa7835bf0b
rastislav.halko <rastislav@bysquare.com>
2024-06-07 10:17:08 +0200

    spring boot init

---
+++ src/main/java/eu/ibacz/moviedatabase/MoviedatabaseApplication.java
@@ -0,0 +1,13 @@
+package eu.ibacz.moviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedatabaseApplication.class, args);
+	}
+
+}
